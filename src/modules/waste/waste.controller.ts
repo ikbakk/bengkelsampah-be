d454a4b7364627a5b9899446861c9330be8b48db
ikbakk <ikbakfir@gmail.com>
2024-10-02 11:21:51 +0800

    Update waste

--- src/modules/waste/waste.controller.ts
+++ src/modules/waste/waste.controller.ts
@@ -6,16 +6,20 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { WasteService } from './waste.service';
 import { CreateWasteDto } from './dto/createWaste.dto';
 import { UpdateWasteDto } from './dto/updateWaste.dto';
+import { JwtGuard } from '../guards/jwt/jwt.guard';
+import { OwnUserGuard } from '../guards/ownUser/ownUser.guard';
 
 @Controller('wastes')
 export class WasteController {
   constructor(private readonly wasteService: WasteService) {}
 
   @Post()
+  @UseGuards(JwtGuard, OwnUserGuard)
   create(@Body() createWasteDto: CreateWasteDto) {
     return this.wasteService.create(createWasteDto);
   }
@@ -31,11 +35,13 @@ export class WasteController {
   }
 
   @Patch(':id')
+  @UseGuards(JwtGuard, OwnUserGuard)
   update(@Param('id') id: string, @Body() updateWasteDto: UpdateWasteDto) {
     return this.wasteService.update(id, updateWasteDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtGuard, OwnUserGuard)
   remove(@Param('id') id: string) {
     return this.wasteService.remove(id);
   }
